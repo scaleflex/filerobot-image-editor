4ba4ecd85cf3efd6ab28f135e26f03fee456cee4
dmitry <stremous@list.ru>
2018-01-19 15:58:55 +0300

    creating npm package like app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
+import ImageEditor from 'components/image-editor-react';
 import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Header = styled.header`
+  text-align: center;
+  background: blue;
+  padding: 20px;
+`;
+
+const Image = styled.img`
+  width: 140px;
+`;
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Header>
+          <Image src={logo} alt="logo"/>
+          <h1 className="App-title">Welcome to React 1</h1>
+        </Header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <ImageEditor>Super great :)</ImageEditor>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { AppContainer } from 'react-hot-loader'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root'),
+  )
+};
+
+render(App);
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    console.log('hi')
+    render(App);
+  })
+}
+
 registerServiceWorker();
