e66ae8533f27d48861d634c58972c0d391f5598c
dmitry <stremous@list.ru>
2018-01-19 06:29:15 +0300

    working on modules

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import { render } from 'react-dom';
+//import React from 'react';
+//import { render } from 'react-dom';
+//import ImageEditor from './ImageEditor';
+//import registerServiceWorker from './registerServiceWorker';
+//import 'cropperjs/dist/cropper.css';
+//
+//render(<ImageEditor />, document.getElementById('image-editor'));
+//
+//registerServiceWorker();
+//
+//export default ImageEditor;
+
 import ImageEditor from './ImageEditor';
-import registerServiceWorker from './registerServiceWorker';
-import 'cropperjs/dist/cropper.css';
 
-render(<ImageEditor />, document.getElementById('image-editor'));
 
-registerServiceWorker();
+export default ImageEditor;
