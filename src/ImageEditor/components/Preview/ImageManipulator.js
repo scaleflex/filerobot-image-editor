cf864163c7f8d05bbd603d847926380ba2254031
dmitry <stremous@list.ru>
2018-01-21 12:10:39 +0300

    working on image editor

--- src/ImageEditor/components/Preview/ImageManipulator.js
+++ src/ImageEditor/components/Preview/ImageManipulator.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 //const Caman = require('caman').Caman;
-const { Image } = require('canvas');
+//const { Image } = require('canvas');
 
 export default class ImageManipulator extends Component {
   shouldComponentUpdate() {
@@ -8,21 +8,41 @@ export default class ImageManipulator extends Component {
   }
 
   componentDidMount() {
-    //Caman('#scaleflex-image-edit-box', function () {
-    //  //this.brightness(10);
-    //  //this.contrast(30);
-    //  //this.sepia(60);
-    //  //this.saturation(-30);
-    //  //this.render();
+    console.log(window.Caman)
+    //window.Caman('#scaleflex-image-edit-box', function () {
+    //  debugger;
+    //  this.brightness(5).render();
     //});
+    var canvas = document.getElementById('scaleflex-image-edit-box');
+    var ctx = canvas.getContext('2d');
+
+    /* Enable Cross Origin Image Editing */
+    var img = new Image();
+    img.crossOrigin = '';
+    img.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/koala.jpg';
+
+    img.onload = function() {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0, img.width, img.height);
+    }
+
+    var $brightness = document.getElementById('brightnessbtn');
+
+    /* In built filters */
+    $brightness.addEventListener('click', function(e) {
+      window.Caman('#scaleflex-image-edit-box', function() {
+        this.brightness(30).render();
+      });
+    });
   }
 
   render() {
     return (
-      <img
-        id="scaleflex-image-edit-box"
-        src="//jolipage.api.airstore.io/v1/get/_/9b9443fd-810a-5540-9672-cd911c1c6c40/main_photo_2.jpg"
-      />
+      <div>
+        <canvas id="scaleflex-image-edit-box"/>
+        <button id="brightnessbtn">Brightness</button>
+      </div>
     )
   }
 }
