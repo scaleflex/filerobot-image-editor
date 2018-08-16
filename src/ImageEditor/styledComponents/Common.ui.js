add049f9f6aea500c0f68e51fb94923ad160d73d
dmitry <dmitry@scaleflex.cloud>
2018-08-16 15:44:55 +0300

    add overlay component which forbids clicking

---
+++ src/ImageEditor/styledComponents/Common.ui.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const NoClickOverlay = styled.div`
+  position: absolute;
+  z-index: 999;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  cursor: wait;
+`;
