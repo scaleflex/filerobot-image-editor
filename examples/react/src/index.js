ba43fff9bf4f43fc17ae24b3b2b3d65da6854b72
dmitry <dmitry@scaleflex.cloud>
2019-06-17 13:10:30 +0300

    updating demo

--- examples/react/src/index.js
+++ examples/react/src/index.js
@@ -1,56 +1,83 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import FilerobotImageEditor from '../../../projects/react';
 
 
-const config = {
-  filerobotUploadKey: '7cc1f659309c480cbc8a608dc6ba5f03',
-  filerobotContainer: 'scaleflex-tests-v5a',
-  // elementId: '',
-  // uploadParams: {},
+const App = () => {
+  const src = 'https://scaleflex.airstore.io/demo/stephen-walker-unsplash.jpg';
+  const [show, toggle] = useState(false);
+
+
+  return (
+    <div>
+      <h1>Filerobot Image Editor</h1>
+
+      <img src={src} onClick={() => { toggle(true) }} alt="example image"/>
+
+      <FilerobotImageEditor
+        show={show}
+        src={src}
+        onClose={() => { toggle(false) }}
+      />
+    </div>
+  )
 };
 
-class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      isShow: false,
-      imgSrc: '//scaleflex.ultrafast.io/https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/koala.jpg'
-    }
-  }
-
-  showImageEditor = () => {
-    this.setState({ isShow: true });
-  }
-
-  onComplete = (newUrl) => {
-    this.setState({ imgSrc: newUrl });
-  }
-
-  onClose = () => {
-    this.setState({ isShow: false });
-  }
-
-  render() {
-    const { imgSrc, isShow } = this.state;
-
-    return (
-      <div>
-        <h1>Filerobot Image Editor</h1>
-
-        <img src={imgSrc} onClick={this.showImageEditor} alt="example image"/>
-
-        <FilerobotImageEditor
-          show={isShow}
-          src={imgSrc}
-          config={config}
-          onComplete={this.onComplete}
-          onClose={this.onClose}
-        />
-      </div>
-    )
-  }
-}
-
 render(<App/>, document.getElementById('app'));
+
+//import React, { Component } from 'react';
+//import { render } from 'react-dom';
+//import FilerobotImageEditor from '../../../projects/react';
+
+
+//const config = {
+//  filerobotUploadKey: '7cc1f659309c480cbc8a608dc6ba5f03',
+//  filerobotContainer: 'scaleflex-tests-v5a',
+//  // elementId: '',
+//  // uploadParams: {},
+//};
+
+//class App extends Component {
+//  constructor() {
+//    super();
+//
+//    this.state = {
+//      isShow: false,
+//      imgSrc: '//scaleflex.ultrafast.io/https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/koala.jpg'
+//    }
+//  }
+//
+//  showImageEditor = () => {
+//    this.setState({ isShow: true });
+//  }
+//
+//  //onComplete = (newUrl) => {
+//  //  this.setState({ imgSrc: newUrl });
+//  //}
+//
+//  onClose = () => {
+//    this.setState({ isShow: false });
+//  }
+//
+//  render() {
+//    const { imgSrc, isShow } = this.state;
+//
+//    return (
+//      <div>
+//        <h1>Filerobot Image Editor</h1>
+//
+//        <img src={imgSrc} onClick={this.showImageEditor} alt="example image"/>
+//
+//        <FilerobotImageEditor
+//          show={isShow}
+//          src={imgSrc}
+//          //config={config}
+//          //onComplete={this.onComplete}
+//          onClose={this.onClose}
+//        />
+//      </div>
+//    )
+//  }
+//}
+//
+//render(<App/>, document.getElementById('app'));
