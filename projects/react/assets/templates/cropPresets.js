e3f79f5ffd2a341a4400bcf1ec6117f6067620c4
dmitry <dmitry@scaleflex.cloud>
2019-09-10 12:06:25 +0300

    add example templates

---
+++ projects/react/assets/templates/cropPresets.js
@@ -0,0 +1,9 @@
+export default [
+  { name: 'original', value: 0 },
+  { name: 'square', value: 1 },
+  { name: 'banner', value: 7.8 },
+  { name: '5 : 4', value: 1.25 },
+  { name: '4 : 3', value: 1.33333 },
+  { name: '6 : 4', value: 1.5 },
+  { name: '16 : 9', value: 1.7777 }
+];
---
+++ projects/react/assets/templates/resizePresets.js
@@ -0,0 +1,15 @@
+export default [
+  { name: 'big square', width: 600, height: 600, ratio: 1 },
+  { name: 'middle square', width: 400, height: 400, ratio: 1 },
+  { name: 'small square', width: 200, height: 200, ratio: 1 },
+  { name: 'small size', width: 1200, height: 960, ratio: 1.25 },
+  { name: 'better quality', width: 1920, height: 1536, ratio: 1.25 },
+  { name: 'small size', width: 1200, height: 900, ratio: 1.33333 },
+  { name: 'better quality', width: 1920, height: 1440, ratio: 1.33333 },
+  { name: 'small size', width: 1200, height: 800, ratio: 1.5 },
+  { name: 'better quality', width: 1920, height: 1280, ratio: 1.5 },
+  { name: 'small size', width: 1200, height: 675, ratio: 1.7777 },
+  { name: 'better quality', width: 1920, height: 1080, ratio: 1.7777 },
+  { name: 'small banner', width: 468, height: 60, ratio: 7.8 },
+  { name: 'big banner', width: 936, height: 120, ratio: 7.8 }
+];
