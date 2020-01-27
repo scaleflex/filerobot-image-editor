f22654e00e4ca176aed223e7ed41d93e85b1ef38
dmitry <dmitry@scaleflex.cloud>
2020-01-27 15:06:54 +0200

    refactor: remove old code

--- projects/react/utils/global.utils.js
+++ projects/react/utils/global.utils.js
@@ -1,5 +1,8 @@
 export const getCanvasNode = (id = 'scaleflex-image-edit-box') => window.document.getElementById(id);
 
+export const getBaseAPI = (baseAPI, container, platform) =>
+  baseAPI ? baseAPI + '/' : getBaseUrl(container, platform);
+
 export const getBaseUrl = (container, platform = 'filerobot') =>  platform === 'filerobot' ?
   `https://api.filerobot.com/${container}/v3/` :
   `https://${container}.api.airstore.io/v1/`;
