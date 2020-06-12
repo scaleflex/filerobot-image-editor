e37e78192a23fd8e836d86feb2d0254f7a8a43ae
Ahmed Khallaf <ahmed@scaleflex.com>
2020-06-12 02:32:55 +0200

    Feat: Adding english translation for modal fullscreen and close buttons

--- projects/react/assets/i18n/en.js
+++ projects/react/assets/i18n/en.js
@@ -1,5 +1,7 @@
 export default {
   'header.image_editor_title': 'Filerobot Image Editor',
+  'header.toggle_fullscreen': 'Toggle fullscreen',
+  'header.close_modal': 'Close window',
   'toolbar.download': 'Download',
   'toolbar.save': 'Save',
   'toolbar.apply': 'Apply',
