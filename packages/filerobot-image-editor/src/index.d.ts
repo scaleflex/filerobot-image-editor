fcc37822f87bb262269436cd86b2c4add527b938
Ahmed Mostafa <ahmed.mostafa1198@gmail.com>
2021-12-08 13:38:13 +0200

    Update typescript types

--- packages/filerobot-image-editor/src/index.d.ts
+++ packages/filerobot-image-editor/src/index.d.ts
@@ -2,8 +2,8 @@ import { FilerobotImageEditorConfig, TABS, TOOLS } from 'react-filerobot-image-e
 
 /*~ Write your module's methods and properties in this class */
 declare class FilerobotImageEditor {
-  TABS: TABS;
-  TOOLS: TOOLS;
+  TABS: typeof TABS;
+  TOOLS: typeof TOOLS;
   constructor(container: HTMLElement, config: FilerobotImageEditorConfig);
   render(additionalConfig?: FilerobotImageEditorConfig): void;
   terminate(): void;
