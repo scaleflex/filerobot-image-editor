6dc92f5eb191dc51a3570e5a2024cc70204eabc3
pospi <sam.pospischil@acer.org>
2024-03-13 08:38:24 +1100

    export all components used in AssemblyPoint directly, to allow custom UI creation wrapping the Canvas & faux-store

--- packages/react-filerobot-image-editor/src/index.js
+++ packages/react-filerobot-image-editor/src/index.js
@@ -2,6 +2,31 @@
 import FilerobotImageEditor from 'components/AssemblyPoint';
 import { TABS_IDS, TOOLS_IDS } from 'utils/constants';
 
-export { TABS_IDS as TABS, TOOLS_IDS as TOOLS };
+import ThemeProvider from '@scaleflex/ui/theme';
+
+/** 'Internal' Dependencies, now made external to access editor Store directly */
+import MainCanvas from 'components/MainCanvas';
+import App from 'components/App';
+import { AppProvider } from 'context';
+import defaultConfig from 'context/defaultConfig';
+import {
+  FontsFaces,
+  OverrideDefaultStyles,
+} from 'components/AssemblyPoint/globalStyles';
+
+import { useStore } from 'hooks';
+
+export {
+  useStore,
+  defaultConfig,
+  ThemeProvider,
+  FontsFaces,
+  OverrideDefaultStyles,
+  AppProvider,
+  App,
+  MainCanvas,
+  TABS_IDS as TABS,
+  TOOLS_IDS as TOOLS,
+};
 
 export default FilerobotImageEditor;
