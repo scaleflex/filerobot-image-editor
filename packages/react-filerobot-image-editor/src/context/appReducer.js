1e8475533698ba0ed19abe29b5fad7891cbc0f39
pospi <sam.pospischil@acer.org>
2024-03-13 14:22:52 +1100

    add debug output to editor reducer when in development mode

--- packages/react-filerobot-image-editor/src/context/appReducer.js
+++ packages/react-filerobot-image-editor/src/context/appReducer.js
@@ -1,8 +1,12 @@
 import actions from 'actions';
 
-const appReducer = (state, action) =>
-  actions[action.type]
+const appReducer = (state, action) => {
+  if (process.env.NODE_ENV !== 'production') {
+    console.info('editor:', action);
+  }
+  return actions[action.type]
     ? actions[action.type](state, action.payload) || state
     : state;
+}
 
 export default appReducer;
