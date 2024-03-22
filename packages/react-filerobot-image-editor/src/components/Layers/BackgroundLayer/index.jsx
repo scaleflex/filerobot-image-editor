7b7f145c825eea65323b957b7e49bf4ba574d657
pospi <sam.pospischil@acer.org>
2024-03-22 15:54:38 +1100

    can only add Layers to root of Konva stage

--- packages/react-filerobot-image-editor/src/components/Layers/BackgroundLayer/index.jsx
+++ packages/react-filerobot-image-editor/src/components/Layers/BackgroundLayer/index.jsx
@@ -1,6 +1,6 @@
 /** External Dependencies */
 import React from 'react';
-import { Rect } from 'react-konva';
+import { Layer, Rect } from 'react-konva';
 
 /** Internal Dependencies */
 import { useStore } from 'hooks';
@@ -8,18 +8,19 @@ import { BACKGROUND_LAYER_ID } from 'utils/constants';
 
 const BackgroundLayer = () => {
   const { canvasWidth, canvasHeight, config } = useStore();
-console.log('!!!', config)
+
   if (!config.fill || config.fill === 'white') return null;
 
   return (
-    <Rect
-      id={BACKGROUND_LAYER_ID}
-      x={0}
-      y={0}
-      width={canvasWidth}
-      height={canvasHeight}
-      fill={config.fill}
-    />
+    <Layer id={BACKGROUND_LAYER_ID}>
+      <Rect
+        x={0}
+        y={0}
+        width={canvasWidth}
+        height={canvasHeight}
+        fill={config.fill}
+      />
+    </Layer>
   );
 };
 
