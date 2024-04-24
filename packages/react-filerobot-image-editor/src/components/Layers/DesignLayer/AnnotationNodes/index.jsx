d00705debc4a2d8d92830a9359dc1c47f4b1264a
pospi <sam.pospischil@acer.org>
2024-04-24 17:55:47 +1000

    begin approach to inject extra annotation into annotations layer
    Comopositing still not working, but progress.

--- packages/react-filerobot-image-editor/src/components/Layers/DesignLayer/AnnotationNodes/index.jsx
+++ packages/react-filerobot-image-editor/src/components/Layers/DesignLayer/AnnotationNodes/index.jsx
@@ -1,17 +1,56 @@
 /** External Dependencies */
-import React, { useMemo } from 'react';
+import React, { useMemo, useRef, useState, useEffect } from 'react';
 
 /** Internal Dependencies */
+import { TOOLS_IDS } from 'utils/constants';
 import { useAnnotationEvents, useStore } from 'hooks';
 import MemoizedAnnotation from './MemoizedAnnotation';
 
+// calling code should fire a `SET_ANNOTATION` editor reducer event with this `payload.id`
+const MAGIC_BLUR_ANNOTATION_ID = "BLUR_RECT";
+
 const AnnotationNodes = () => {
-  const { annotations = {}, selectionsIds = [] } = useStore();
+  const {
+    originalImage,
+    shownImageDimensions,
+    annotations = {}, selectionsIds = [],
+  } = useStore();
+  const allAnnotations = Object.values(annotations);
+
   const annotationEvents = useAnnotationEvents();
+
+  const [clonedImg, setClonedImg] = useState();
+
+  useEffect(() => {
+    const img = document.createElement('img');
+    img.src = originalImage.src;
+    img.onload = () => setClonedImg(img);
+  }, [originalImage]);
+
+  // inject blurred background image to canvas when magic 'blur' annotation is active
+  // if (allAnnotations.find(a => a.id === MAGIC_BLUR_ANNOTATION_ID) !== undefined) {
+  // for testing in stock editor (above condition requires deep consumer component integration)
+  if (allAnnotations.length && clonedImg) {
+    allAnnotations.unshift({
+      name: "Image",
+      id: "image-blur",
+      image: originalImage,
+      x: 0,
+      y: 0,
+      width: shownImageDimensions.width,
+      height: shownImageDimensions.height,
+      filters: [Konva.Filters.Blur],
+      blurRadius: 10,
+      scaleX: 1,
+      scaleY: 1,
+      listening: false,
+      draggable: false,
+    });
+  }
 
   return useMemo(
     () =>
-      Object.values(annotations).map((annotation) => (
+      allAnnotations.map((annotation) => (
         <MemoizedAnnotation
           key={annotation.id}
           annotation={annotation}
@@ -19,7 +58,7 @@ const AnnotationNodes = () => {
           selectionsIds={selectionsIds}
         />
       )),
-    [annotations, annotationEvents, selectionsIds],
+    [allAnnotations, annotationEvents, selectionsIds, clonedImg],
   );
 };
 
