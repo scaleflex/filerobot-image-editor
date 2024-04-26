cd45951e997c8a8d69f7d523719103aa27108192
pospi <sam.pospischil@acer.org>
2024-04-26 13:10:16 +1000

    remove unnecessary cloned image, simplify Annotation blur background activation
    (it's used in all annotation tools for all intents & purposes in NAP-ICTL scenario)

--- packages/react-filerobot-image-editor/src/components/Layers/DesignLayer/AnnotationNodes/index.jsx
+++ packages/react-filerobot-image-editor/src/components/Layers/DesignLayer/AnnotationNodes/index.jsx
@@ -1,8 +1,7 @@
 /** External Dependencies */
-import React, { useMemo, useRef, useState, useEffect } from 'react';
+import React, { useMemo } from 'react';
 
 /** Internal Dependencies */
-import { TOOLS_IDS } from 'utils/constants';
 import { useAnnotationEvents, useStore } from 'hooks';
 import MemoizedAnnotation from './MemoizedAnnotation';
 
@@ -19,18 +18,10 @@ const AnnotationNodes = () => {
 
   const annotationEvents = useAnnotationEvents();
 
-  const [clonedImg, setClonedImg] = useState();
-
-  useEffect(() => {
-    const img = document.createElement('img');
-    img.src = originalImage.src;
-    img.onload = () => setClonedImg(img);
-  }, [originalImage]);
-
   // inject blurred background image to canvas when magic 'blur' annotation is active
   // if (allAnnotations.find(a => a.id === MAGIC_BLUR_ANNOTATION_ID) !== undefined) {
   // for testing in stock editor (above condition requires deep consumer component integration)
-  if (allAnnotations.length && clonedImg) {
+  if (allAnnotations.length) {
     allAnnotations.unshift({
       name: "Image",
       id: "image-blur",
@@ -58,7 +49,7 @@ const AnnotationNodes = () => {
           selectionsIds={selectionsIds}
         />
       )),
-    [allAnnotations, annotationEvents, selectionsIds, clonedImg],
+    [allAnnotations, annotationEvents, selectionsIds],
   );
 };
 
