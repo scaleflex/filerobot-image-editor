61f0038c3938e899f1284f397e3335f7551f17e3
Ahmed Mostafa <ahmed.mostafa1198@gmail.com>
2024-06-05 04:41:57 +0300

    Consider bgColor while checking if same source provided or not

--- packages/react-filerobot-image-editor/src/utils/isSameSource.js
+++ packages/react-filerobot-image-editor/src/utils/isSameSource.js
@@ -4,7 +4,8 @@ const isSameSource = (source1, source2) =>
   (((source1 instanceof HTMLImageElement || typeof source1 === 'object') &&
     source1.src === source2.src &&
     source1.width === source2.width &&
-    source1.height === source2.height) ||
+    source1.height === source2.height &&
+    source1.bgColor === source2.bgColor) ||
     (source1?.src || source1) === source2.src);
 
 export default isSameSource;
