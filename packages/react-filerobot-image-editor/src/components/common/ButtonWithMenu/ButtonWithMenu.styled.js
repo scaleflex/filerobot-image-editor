52f6aabff94fcf4d0434043655da44fbc642b23c
Amr Hamdy <amrelbialy26@gmail.com>
2024-09-10 13:25:32 +0300

    refactor: update arrow styles in save button

--- packages/react-filerobot-image-editor/src/components/common/ButtonWithMenu/ButtonWithMenu.styled.js
+++ packages/react-filerobot-image-editor/src/components/common/ButtonWithMenu/ButtonWithMenu.styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import Button from '@scaleflex/ui/core/button';
 import { Color as PC } from '@scaleflex/ui/utils/types/palette/color';
 import { Menu, MenuItem, MenuItemIcon } from '@scaleflex/ui/core';
-import { Arrow } from '@scaleflex/icons';
+import { ArrowBottom } from '@scaleflex/icons';
 
 const StyledButtonWrapper = styled.div`
   display: flex;
@@ -37,9 +37,9 @@ const StyledMenuIcon = styled(MenuItemIcon)`
   align-items: center;
 `;
 
-const StyledArrow = styled(Arrow)`
+const StyledArrow = styled(ArrowBottom)`
   margin-top: ${({ buttonSize }) => buttonSize === 'md' && '1px'};
-  transform: ${({ open }) => (open ? `rotate(-90deg)` : `rotate(90deg)`)};
+  transform: ${({ open }) => open && `rotate(-180deg)`};
 `;
 
 export {
