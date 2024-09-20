814f60e03b88ada8a175fdaf618924fdaa528b84
Ahmed Mostafa <ahmed.mostafa1198@gmail.com>
2024-09-20 16:02:21 +0300

    Add empty object if no Konva.Filters yet

--- packages/react-filerobot-image-editor/src/utils/assignFinetuneNamesToKonva.js
+++ packages/react-filerobot-image-editor/src/utils/assignFinetuneNamesToKonva.js
@@ -1,5 +1,5 @@
 const assignFinetuneNamesToKonva = () => {
-  Object.keys(Konva.Filters).forEach((key) => Konva.Filters[key].finetuneName = key)
+  Object.keys(Konva.Filters || {}).forEach((key) => Konva.Filters[key].finetuneName = key)
 }
 
 export default assignFinetuneNamesToKonva;
