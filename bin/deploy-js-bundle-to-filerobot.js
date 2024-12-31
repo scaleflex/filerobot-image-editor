9d91f5bb02f9619ad24cd62ce1c77f9d81d5e1d4
Ahmed Mostafa <ahmed.mostafa1198@gmail.com>
2025-01-01 00:10:49 +0200

    Refactor: update version source to lerna.json and remove unnecessary content-type header

--- bin/deploy-js-bundle-to-filerobot.js
+++ bin/deploy-js-bundle-to-filerobot.js
@@ -5,7 +5,7 @@ const { createReadStream } = require('fs');
 const { join } = require('path');
 const {
   version: originalVersion,
-} = require('../packages/filerobot-image-editor/package.json');
+} = require('../lerna.json');
 
 const splittedVersion = originalVersion.split('.');
 const version = splittedVersion
@@ -58,7 +58,6 @@ const deploy = (sassKey, uploadToSubFolder) => {
     method: 'POST',
     body: formData,
     headers: {
-      'Content-Type': 'multipart/form-data',
       'X-Filerobot-Key': sassKey,
     },
   })
