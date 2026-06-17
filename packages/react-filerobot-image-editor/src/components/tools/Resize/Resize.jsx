5208fe9c66dfe3ecf843849c56ceef9eabbd2e89
Chris Paganon <info@chrispaganon.com>
2026-06-17 12:01:53 +0200

    fix: resize uses cropped aspect ratio

--- packages/react-filerobot-image-editor/src/components/tools/Resize/Resize.jsx
+++ packages/react-filerobot-image-editor/src/components/tools/Resize/Resize.jsx
@@ -9,7 +9,6 @@ import { Reset } from '@scaleflex/icons';
 import { SET_RESIZE, ZOOM_CANVAS } from 'actions';
 import { useStore } from 'hooks';
 import getProperDimensions from 'utils/getProperDimensions';
-import getSizeAfterRotation from 'utils/getSizeAfterRotation';
 import getZoomFitFactor from 'utils/getZoomFitFactor';
 import restrictNumber from 'utils/restrictNumber';
 import { DEFAULT_ZOOM_FACTOR } from 'utils/constants';
@@ -49,12 +48,6 @@ const Resize = ({ onChange, currentSize, hideResetButton, alignment }) => {
       originalImage.height * 10,
     );
 
-    const originalImgSizeAfterRotation = getSizeAfterRotation(
-      originalImage.width,
-      originalImage.height,
-      rotation,
-    );
-
     const isHeight = name === 'height';
     const secondDimensionName = isHeight ? 'width' : 'height';
 
@@ -65,12 +58,10 @@ const Resize = ({ onChange, currentSize, hideResetButton, alignment }) => {
 
     const isRatioUnlocked = currentSize.ratioUnlocked ?? resize.ratioUnlocked;
     if (!isRatioUnlocked) {
-      const originalImgRatio =
-        originalImgSizeAfterRotation.width /
-        originalImgSizeAfterRotation.height;
+      const currentImgRatio = dimensions.width / dimensions.height;
       newResize[secondDimensionName] = isHeight
-        ? Math.round(newResize[name] * originalImgRatio)
-        : Math.round(newResize[name] / originalImgRatio);
+        ? Math.round(newResize[name] * currentImgRatio)
+        : Math.round(newResize[name] / currentImgRatio);
     }
 
     if (
